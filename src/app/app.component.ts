cc3979669433a64a40988c93b5dcee049e8ce04e
olivier <olivierpaul@macbook-pro-de-olivier-1.home>
2018-03-26 21:55:27 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {PostListItemComponentComponent} from "./post-list-item-component/post-list-item-component.component";
+import {post} from "selenium-webdriver/http";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+}
---
+++ src/app/post-list-component/post-list-component.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list-component',
+  templateUrl: './post-list-component.component.html',
+  styleUrls: ['./post-list-component.component.scss']
+})
+export class PostListComponentComponent implements OnInit {
+  post1 = {title: 'Mon Premier post',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    loveIts: 0,
+    created_at: new Date()
+  };
+  post2 = {title: 'Mon deuximème post',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    loveIts: 0,
+    created_at: new Date()
+  };
+
+  post3 =  {title: 'Encore un post',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    loveIts: 0,
+    created_at: new Date()
+  };
+  posts = [this.post1, this.post2, this.post3];
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
